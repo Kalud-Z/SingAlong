3bfa35ff4139f90ebf5a5a6ceee3265c45ba6235
Kalud.Z <khaled.fhm@live.fr>
2020-06-06 13:51:09 +0200

    linked video controls to keyboard keys.

--- src/app/youtube/youtube.component.ts
+++ src/app/youtube/youtube.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, ViewChild, ViewChildren, QueryList, Input, HostListe
 import { DataService } from '../_services/data.service';
 import { videoObj } from './video.model';
 import { DomSanitizer} from '@angular/platform-browser';
-import { YouTubePlayer } from '@angular/youtube-player';
+// import { YouTubePlayer } from '@angular/youtube-player';
 
 
 
@@ -64,39 +64,60 @@ export class YoutubeComponent implements OnInit {  //###########################
     this.initAPI()
   }
 
+
   //I had a pipe that does this. But when i use in the iframe . the video frame loses its controls ! (only pause works !)
   // when I remove the pipe. everything works fine.
   private makeUrlSafe(url : string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
-  // §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§  YOUTUBE IFRAME API  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  // §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§  YOUTUBE IFRAME API  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
   setCalculatedPlayerWidth(event) {
     this.playerWidth = event;
     this.playerHeight = this.playerWidth / 1.777
   }
 
-  // YT: YouTubePlayer;
   afterFirstVideo = false;
   YT: any;
   videoID: string;
   player: any;
+  videoState = 1; //= 2 ==> playin // = 1 ==> paused
+
 
   // keyCode = 39  arrow right
   // keyCode = 37  arrow left
   // keyCode = 38  arrow up
   // keyCode = 40  arrow down
+  // keyCode = 32  space
 
   @HostListener('document:keydown', ['$event']) handleKeyboardEvent(event: KeyboardEvent) { 
+    // console.log(event)
+
     if(event.keyCode === 39) {  //we skip forward
-      this.player.seekTo( this.player.getCurrentTime() + 5, true)
+      this.player.seekTo(this.player.getCurrentTime() + 5, true)
     }
 
-    if(event.keyCode === 37) {  //we skip forward
-      this.player.seekTo( this.player.getCurrentTime() - 5, true)
+    if(event.keyCode === 37) {  //we skip backward
+      this.player.seekTo(this.player.getCurrentTime() - 5, true)
     }
 
+    if(event.keyCode === 32) {  //we pause
+
+       if(event.target === document.body) {
+        event.preventDefault();
+        }
+
+      if(this.videoState == 1) {
+        this.player.playVideo();
+        setTimeout(() => { this.videoState = 2 }, 50);
+      }
+      if(this.videoState == 2) {
+        this.player.pauseVideo();
+        setTimeout(() => { this.videoState = 1 }, 50);
+      }
+
+    }
   }
         
 
@@ -111,8 +132,6 @@ export class YoutubeComponent implements OnInit {  //###########################
   playerHeight : number
 
 
-
-  /* 2. Initialize method for YT IFrame API */
   initAPI() {
     // Return if Player is already created . in case we wanna play another video . without refreshing the app.
     if (window['YT']) {
@@ -125,7 +144,7 @@ export class YoutubeComponent implements OnInit {  //###########################
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    /* 3. initVideoPlayer() will create an <iframe> (and YouTube player) after the API code downloads. */
+    /* initVideoPlayer() will create an <iframe> (and YouTube player) after the API code downloads. */
     window['onYouTubeIframeAPIReady'] = () => this.initVideoPlayer();
   }
 
@@ -136,42 +155,41 @@ export class YoutubeComponent implements OnInit {  //###########################
       // videoId: 'tNy8Y7BhUeI',
       height: "100%",
       width: "100%",
-      // playerVars: {
-      //   autoplay: 0,
-      //   modestbranding: 1,
-      //   controls: 1,
-      //   disablekb: 1,
-      //   rel: 0,
-      //   showinfo: 0,
-      //   fs: 0,
-      //   playsinline: 1
-      // },
-      // events: { 'onReady'  : this.onPlayerReady.bind(this) }
-      events: { 'onReady'  : this.onPlayerReady}
+      playerVars: {   //these parameters , most of them aint working properly !!!
+        autoplay: 0,
+        modestbranding: 1,
+        controls: 1,
+        disablekb: 1,
+        rel: 0,
+        showinfo: 0,
+        fs: 0,
+        playsinline: 1
+      },
+      events: {
+        'onReady'  : this.onPlayerReady
+        // 'onStateChange' : this.onPlayerStateChange,
+      }
     });
+
   }
 
   onPlayerReady(event) {
     event.target.playVideo();
+    setTimeout(() => { event.target.pauseVideo() }, 1000)
   }
 
-  
 
 
-  cleanTime() {
-    return Math.round(this.player.getCurrentTime())
-  };
-
-
+// tried to assign the state value from the event , to a property . it didnt work !! (so I had to keep track of the state manually)
+  // onPlayerStateChange(event) {
+    // console.log(event)
+    // this.changeVideoState(event.data)
+  //   this.videoState = event.data
+  // };
 
 
 }  //#########################################################################################################################################################
 // ###########################################################################################################################################################
-
-
-
-
-
 
 
 
