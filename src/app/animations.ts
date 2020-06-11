056faa4b26ed2f9d77f7170ae5b9baa0267ef583
Kalud.Z <khaled.fhm@live.fr>
2020-06-11 11:35:20 +0200

    adjusted animations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,41 +1,8 @@
 import { trigger, transition, group, query, style, animate, stagger } from '@angular/animations';
 
 
-export const lyricsFullScreenTrigger = trigger('lyricsFullScreenState', [
-    transition('default => fullScreen' , [
-        group([
-            query('app-youtube' , [
-                animate('.2s' , style({
-                    transform : 'scale(0.9)'
-                })),
-                animate('0.2s')
-            ] , { optional: true }), //query
 
-            // query('app-lyrics' , [
-            //     // animate('2s' , style({
-            //     //     'align-self': 'flex-end',
-            //     //     width : '46vw !important'
-            //     // })),
-            //     style({
-            //         'align-self': 'flex-end',
-            //         width : '46vw !important'
-            //     }),
-            //     // animate('.1s' , style({
-            //     //     transform : 'scaleX(0.5)'
-            //     // })),
-            //     animate(2000 , style({
-            //         'align-self': 'center',
-            //         width : '95vw !important'
-            //     }))
-            //     // animate('2s')
-            // ] , { optional: true }), //query
-        ])
-        
-      ]), //transition
-])
-
-
-
+// Video Stuff ###########################################################################################
 
 export const exitLogoTrigger = trigger('exitLogoState', [
     transition(':enter' , [
@@ -57,19 +24,45 @@ export const displayVideoSuggestionsTrigger = trigger('displayVideoSuggestionsSt
   
     ]),  //enter
 
-
     transition(':leave' , [
-        query('.videosSuggestions__suggestion' , animate('.2s' , style({
-            opacity : 0,
-            transform : 'translateX(-300%)'
-        })) , { optional : true })
-      
-    ]),  //leave    
+        animate('1s 0.5s ease-in' , style({
+            transform : 'translateX(-300%)',
+            opacity : 0
+        }))
 
+    ]),  //leave    
 
 ])
 
 
+
+export const iframeOuterContainerTrigger = trigger('iframeOuterContainerState' , [
+    transition('hidden => shown' , [
+       style({
+           opacity : 0
+       }),
+       animate('.3s 1.5s ease-out' , style({
+           opacity : 1
+       }))
+    ]),  //enter
+
+    transition(':leave' , [
+        // animate('1s 0.5s ease-in' , style({
+        //     transform : 'translateX(-300%)',
+        //     opacity : 0
+        // }))
+
+    ]),  //leave    
+
+])
+
+
+
+
+
+
+
+// Lyrics stuff ##########################################################################################
 
 export const displayLyricsSuggestionsTrigger = trigger('displayLyricsSuggestionsState' , [
     transition(':enter' , [
@@ -81,18 +74,13 @@ export const displayLyricsSuggestionsTrigger = trigger('displayLyricsSuggestions
     ]),  //enter
 
     transition(':leave' , [
-        query('.lyricsSuggestions__suggestion' , [
-            stagger('120ms' , animate('2.12s ease-in' , style({ 
-                opacity : 0,
-                transform : 'translateX(200%)'
-            })))
-        ] , { optional : true }), 
+        animate('1s 0.5s ease-in' , style({
+            transform : 'translateX(300%)',
+            opacity : 0
+        }))
 
     ]),  //leave    
 ])
-
-
-
 
 
 export const displayChosenLyricTrigger = trigger('displayChosenLyricState' , [
@@ -101,7 +89,7 @@ export const displayChosenLyricTrigger = trigger('displayChosenLyricState' , [
           opacity : 0,
           transform : 'translateY(50%)'
       }),
-      animate('5.5s  ease-out' , style({
+      animate('2s 1.6s ease-out' , style({
         opacity : 1,
         transform : 'translateY(0%)'
       }))
@@ -130,6 +118,21 @@ export const displayChosenLyricTrigger = trigger('displayChosenLyricState' , [
         // ] , { optional : true })
     
   ]) //enter
+])
+
+
+export const lyricsFullScreenTrigger = trigger('lyricsFullScreenState', [
+    transition('default => fullScreen' , [
+        group([
+            query('app-youtube' , [
+                animate('.2s' , style({
+                    transform : 'scale(0.9)'
+                })),
+                animate('0.2s')
+            ] , { optional: true }), //query
+        ])
+        
+      ]), //transition
 ])
 
 
