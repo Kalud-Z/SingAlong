a8c2dc34e6d869f1e052cbf28820c4f546ce0f92
kalud <khaled.fhm@live.fr>
2021-02-23 12:00:56 +0100

    minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   // APIKey : 'AIzaSyD1fHEPYk2Si2RHx1vTCQc3N5wNiEhSgSc'
-  YoutubeAPIKey : 'AIzaSyAFfKaZQkOjrsUMgJYfix3urmR6mpOYVH8',
+  YoutubeAPIKey : 'AIzaSyDLCxhgbEJC5TTYNcAT53NzBXXB5_WZjag', //new project
+
   LyricsAPIKey : 'B22Vz3RGZBYB1fYyGcIzFa2V5L1yZhw6ZmqiWWw4yidVvVl3rbm9DkKqPP9wfmi4'
-  
+
 };
